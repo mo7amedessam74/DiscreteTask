f8fe7fe7f3590709bb66cacde964787588f45e62
mo7amedessam74 <121492859+mo7amedessam74@users.noreply.github.com>
2022-12-27 13:32:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Frist Number:");
+            int sum1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Second Number:");
+            int sum2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Perfect Resualt:");
+            {
+                if (sum1 >= 0 && sum2 >= 6 && sum1 < 7)
+                    Console.WriteLine("6");
+                if (sum2 >= 28 && sum1 <= 28)
+                    Console.WriteLine("28");
+                if (sum2 >= 496 && sum1 <= 496)
+                    Console.WriteLine("496");
+                if (sum2 >= 8128 && sum1 <= 8128)
+                    Console.WriteLine("8128");
+                if (sum2 >= 33550336 && sum1 <= 33550336)
+                    Console.WriteLine("33550336");
+            }
+        }
+    }
+}
